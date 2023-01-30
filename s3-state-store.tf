3ad141210ca92ba47448b7c8e04d353df71b50df
shaly.c@gmail.com <shaly.c@gmail.com>
2023-01-30 15:57:02 -0500

    Splitting dynamo db out of the state store tf file

--- s3-state-store.tf
+++ s3-state-store.tf
@@ -35,18 +35,3 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = var.block_public_access
   restrict_public_buckets = var.block_public_access
 }
-
-resource "aws_dynamodb_table" "terraform_state_locks" {
-  name         = var.dynamodb_table_name
-  billing_mode = var.dynamodb_billing_mode
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = var.dynamodb_table_name
-  }
-}
